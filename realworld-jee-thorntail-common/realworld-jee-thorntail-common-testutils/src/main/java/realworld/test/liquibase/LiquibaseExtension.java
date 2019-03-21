3e03b79378914cd7b4d4916e734e159bc1eeaddf
Nikos Paraskevopoulos <nikospara@yahoo.com>
2019-03-22 00:30:08 +0200

    Reducing the verbosity of Liquibase in tests

--- realworld-jee-thorntail-common/realworld-jee-thorntail-common-testutils/src/main/java/realworld/test/liquibase/LiquibaseExtension.java
+++ realworld-jee-thorntail-common/realworld-jee-thorntail-common-testutils/src/main/java/realworld/test/liquibase/LiquibaseExtension.java
@@ -10,22 +10,29 @@ import liquibase.database.jvm.JdbcConnection;
 import liquibase.resource.ClassLoaderResourceAccessor;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * A JUnit 5 extension that runs Liquibase before all tests.
  */
 public class LiquibaseExtension implements BeforeAllCallback {
+
+	static final Logger LOG = LoggerFactory.getLogger(LiquibaseExtension.class);
+
 	@Override
 	public void beforeAll(ExtensionContext extensionContext) throws Exception {
 		String dburl = System.getProperty("database-test.url");
 		String dbuser = System.getProperty("database-test.username");
 		String dbpass = System.getProperty("database-test.password");
-		System.out.println("*******************************");
-		System.out.println("* Liquibase Junit 5 Extension");
-		System.out.println("* DB URL: " + dburl);
-		System.out.println("* Username: " + dbuser);
-		System.out.println("* Password: " + (dbpass != null ? "****"  : "null"));
-		System.out.println("*******************************");
+		LOG.debug(
+				"**************************************************************" +
+				"* Liquibase Junit 5 Extension" +
+				"* DB URL: " + dburl +
+				"* Username: " + dbuser +
+				"* Password: " + (dbpass != null ? "****"  : "null") +
+				"**************************************************************"
+		);
 
 		try( Connection conn = DriverManager.getConnection(dburl, dbuser, dbpass) ) {
 			Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(conn));
